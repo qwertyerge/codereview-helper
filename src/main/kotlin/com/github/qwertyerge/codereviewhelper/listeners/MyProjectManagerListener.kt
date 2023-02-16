4699574cc01c69519fb9c9f687f531c0f05c9d77
GitHub Action <action@github.com>
2023-02-16 09:29:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/qwertyerge/codereviewhelper/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/qwertyerge/codereviewhelper/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.qwertyerge.codereviewhelper.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.qwertyerge.codereviewhelper.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/qwertyerge/codereviewhelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/qwertyerge/codereviewhelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.qwertyerge.codereviewhelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.qwertyerge.codereviewhelper.MyBundle
 
 class MyApplicationService {
 
